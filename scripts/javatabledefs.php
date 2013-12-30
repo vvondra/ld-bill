2d832195848e4ff4a9c6cac5208d7a339aee821c
Vojtech Vondra <vojtavondra@gmail.com>
2013-12-30 16:29:21 +0100

    scripts for generating table definitions

---
+++ scripts/javatabledefs.php
@@ -0,0 +1,27 @@
+<?php
+
+$tables = require "tabledefs.php";
+
+$tpl = <<<JAVA
+package cz.vojtechvondra.ldbill.psp;
+
+public class %sTableDefinition extends TableDefinition {
+    public %sTableDefinition() {
+        tableName = "%s";
+        colNames = new String[] {
+%s
+        };
+    }
+}
+
+JAVA;
+
+foreach ($tables as $table => $cols) {
+	$colDefs = array();
+	foreach ($cols as $col) {
+		$colDefs[] = '            "' . $col . '"';
+	}
+	$colDefs = implode(",\n", $colDefs);
+	$file = sprintf($tpl, ucfirst($table), ucfirst($table), $table, $colDefs);
+	file_put_contents("java/" . ucfirst($table) . "TableDefinition.java", $file);
+}
